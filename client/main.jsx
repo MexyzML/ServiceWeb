cfa4b063b4d8ab970e53ae6ba7fd425be4768592
MexyzML <marin.legrand@epsi.fr>
2021-03-16 16:59:56 +0100

    Retour meteor "vide"

--- client/main.jsx
+++ client/main.jsx
@@ -1,14 +1,8 @@
-Template.home.onCreated(function homeOnCreated() {
-  let ctrl = this;
-  this.movies = new ReactiveVar();
-  HTTP.call('GET', 'http://localhost:3000/api/discover/movie', {},
-  function(error, response) {
-  // Handle the error or response here.
-  ctrl.movies.set(JSON.parse(response.content).results)
-  });
-  });
-  Template.home.helpers({
-  movies() {
-  return Template.instance().movies.get();
-  }
-  });
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { render } from 'react-dom';
+import { App } from '/imports/ui/App';
+
+Meteor.startup(() => {
+  render(<App/>, document.getElementById('react-target'));
+});
--- server/main.js
+++ server/main.js
@@ -1,6 +1,31 @@
-import { localDatas } from './local-datas.js';
-Meteor.startup(() => {});
-WebApp.connectHandlers.use('/api/discover/movie', (req, res, next) => {
-    res.writeHead(200);
-    res.end(JSON.stringify(localDatas));
+import { Meteor } from 'meteor/meteor';
+import { LinksCollection } from '/imports/api/links';
+
+function insertLink({ title, url }) {
+    LinksCollection.insert({ title, url, createdAt: new Date() });
+}
+
+Meteor.startup(() => {
+    // If the Links collection is empty, add some data.
+    if (LinksCollection.find().count() === 0) {
+        insertLink({
+            title: 'Do the Tutorial',
+            url: 'https://www.meteor.com/tutorials/react/creating-an-app'
+        });
+
+        insertLink({
+            title: 'Follow the Guide',
+            url: 'http://guide.meteor.com'
+        });
+
+        insertLink({
+            title: 'Read the Docs',
+            url: 'https://docs.meteor.com'
+        });
+
+        insertLink({
+            title: 'Discussions',
+            url: 'https://forums.meteor.com'
+        });
+    }
 });
