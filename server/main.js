9372d6925ca952705e5abe3ab13540162c32951d
MexyzML <marin.legrand@epsi.fr>
2021-03-16 17:22:26 +0100

    test

--- server/main.js
+++ server/main.js
@@ -1,31 +1,13 @@
 import { Meteor } from 'meteor/meteor';
-import { LinksCollection } from '/imports/api/links';
+import { localDatas } from './local-datas.js';
 
-function insertLink({ title, url }) {
-    LinksCollection.insert({ title, url, createdAt: new Date() });
-}
+
 
 Meteor.startup(() => {
     // If the Links collection is empty, add some data.
-    if (LinksCollection.find().count() === 0) {
-        insertLink({
-            title: 'Do the Tutorial',
-            url: 'https://www.meteor.com/tutorials/react/creating-an-app'
-        });
-
-        insertLink({
-            title: 'Follow the Guide',
-            url: 'http://guide.meteor.com'
-        });
-
-        insertLink({
-            title: 'Read the Docs',
-            url: 'https://docs.meteor.com'
-        });
-
-        insertLink({
-            title: 'Discussions',
-            url: 'https://forums.meteor.com'
-        });
-    }
 });
+
+/*WebApp.connectHandlers.use('/api/discover/movie', (req, res, next) => {
+res.writeHead(200);
+res.end(JSON.stringify(localDatas));
+});*/
